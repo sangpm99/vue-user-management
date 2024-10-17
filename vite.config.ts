5b8186e813c8101e7adf7cb96017fc006a2cff9a
sangpm99 <sangpm72@wru.vn>
2024-10-17 22:05:01 +0700

    Small Update

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify';
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -13,10 +13,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-    extensions: ['.js', '.vue', '.ts', '.json'],
-  },
-  server: {
-    port: Number(process.env.port)
+    }
   }
 })
